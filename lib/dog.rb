8821cf75fd063fcdafba5f848f578902766b02b4
Luke Chen <luke.chen.2010@gmail.com>
2020-05-07 14:58:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+
+class Dog
+  
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(new_name)
+    @name = new_name
+    save
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+end
